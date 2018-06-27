8b847cc301e4cf83a5e88529601aaba8c77c1088
Rajendarreddy Jagapathi <rajendarreddyj@gmail.com>
2018-06-27 11:09:51 -0500

    cleanup and create root and child project structures
    Signed-off-by: Rajendarreddy Jagapathi <rajendarreddyj@gmail.com>

--- reactive-web-example/src/main/java/module-info.java
+++ reactive-web-example/src/main/java/module-info.java
@@ -1,8 +1,9 @@
-module com.rajendarreddyj.reactivewebexample {
+open module com.rajendarreddyj.reactivewebexample {
     requires reactor.core;
     requires spring.web;
     requires spring.context;
     requires spring.webflux;
     requires spring.boot;
     requires spring.boot.autoconfigure;
+    requires java.sql;
 }
